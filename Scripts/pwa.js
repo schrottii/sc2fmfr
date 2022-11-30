23285d231321618d11ce9c72bf0d345310864bbb
fn78 <oh.seyy@yahoo.de>
2022-11-30 20:07:03 +0100

    hmm

--- Scripts/pwa.js
+++ Scripts/pwa.js
@@ -1,10 +1,8 @@
-const btnInstall = document.querySelector("button#install_app");
-const appInfo = document.querySelector("div#install_app_info");
+const btnInstall = document.querySelector("div.absolute button#add-button");
 
 function addPwaInstall() {
     window.addEventListener("beforeinstallprompt", e => {
         btnInstall.style.display = "block";
-        appInfo.style.display = "block";
         btnInstall.addEventListener("click", clickEvent => {
             e.prompt();
             e.userChoice.then(choice => {

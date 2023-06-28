0491bb7ecfd2acd3c84994ee1641ec74affe2af2
frankneurhein <49289338+frankneurhein@users.noreply.github.com>
2023-06-29 01:48:08 +0200

    3.2.2 (#36)
    * Barrel changes, bug fixes (3.2 early) + my first stash uwu
    
    * empty commit
    
    * Loading screen & storm animation
    
    * Supernova animation and a new option
    
    * Fragment buffs, easier special achievements and more
    
    * hbv
    
    * beam converter fix
    
    * mastery / quest progress display
    
    * New Fairy Dust formula???
    
    * Other cool things
    
    * Integral calculation for faster barrels
    
    * Integral: Better Barrels
    
    * Hyper Buy on scrapyard, more Alien Dust
    
    * Integrals for magnets, gs, beams, fragments, dark fragments and dark scrap
    
    * Integrals for: posus, mythus, tire upgrades and all dust upgrades (100% DONE?!)
    
    * some fixes
    
    * fixed 1. ds upg
    
    * Mythus, refunds for emblems
    
    * Fixed hyperbuy 0?
    
    * fixed hyperbuy 0???
    
    * Hyper Buy level cap and percentage, setting
    
    * Some small improvements
    
    * fixed some stuff, maybe
    
    * uglier code, better function
    
    * tried to fix caelum as well
    
    * fixed caelum fr
    
    * buffed 2nd bricks upg, volans, volans2
    
    * fixed 12th tire upg, magnet upgs
    
    * Screws Rework
    
    * some, uh, improvements
    
    * scrapyard thing
    
    * auto buyers now work like hyper buy
    
    * fixed fraggies
    
    * 3.2 DONE
    
    * nerfed it a bit
    
    * 3.2.1 - fixed duplicate and mythus?
    
    * 3.2.1: Much shorter save codes
    
    * Fixed converter exploit?
    
    * test
    
    * new puppis integral?
    
    * New Aquila formula
    
    * nobarrels setting thing
    
    * test
    
    * animation change
    
    * gifts n more
    
    * gift code thing
    
    * friends crash fix
    
    * easier beams
    
    * Hyperbuy scrapyard buff / hyperbuy buttons fixes
    
    * Scrapyard Hyper Buy Reworked
    
    * Hyper Buy for plastic bags
    
    * hyperbuy fixes
    
    * fix?
    
    * what am I even doing...
    
    * dust above infinity
    
    * ???
    
    * fixed stuff
    
    * several things, such as mars max. level
    
    * smol stuff
    
    * fragments fix?
    
    * poop puppis
    
    * 3.2.2 first few thingz
    
    * more fixes ayy
    
    * more than infinity beams in one fall
    
    * Infinity fix
    
    * fixed 7x crash
    
    * glitch beam crash
    
    * storm fix
    
    * fixing it
    
    * 3.2.2 done

--- Scripts/gamedefinition.js
+++ Scripts/gamedefinition.js
@@ -5,7 +5,7 @@ const BARRELS = 1000;
 const CONST_SENDLIMIT = (currentMonth == 11 ? 6 : 3);
 const CONST_OPENLIMIT = (currentMonth == 11 ? 8 : 4);
 
-const gameVersionText = "v3.2.1 (v3.9.1)";
+const gameVersionText = "v3.2.2 (v3.9.2)";
 
 var game =
 {
@@ -260,7 +260,7 @@ var game =
             level => new Decimal(1 + 0.2 * level).mul(Decimal.pow(1.2, Math.max(0, level - 10))),
             {
                 getEffectDisplay: effectDisplayTemplates.numberStandard(1, "x"),
-                integral: level => new Decimal(5).mul(Math.pow(10, 10)).mul((Math.log(11) - Math.log(10)) * (level + 2) - 1).mul(new Decimal(11).pow(level)).div(new Decimal(Math.log(11) - Math.log(10)).pow(2).mul(Math.pow(11, 10)).mul(new Decimal(10).pow(level))).mul(applyUpgrade(game.solarSystem.upgrades.jupiter)),
+                integral: level => new Decimal(5).mul(Math.pow(10, 10)).mul((Math.log(11) - Math.log(10)) * (level + 2) - 1).mul(new Decimal(11).pow(level)).div(new Decimal(Math.log(11) - Math.log(10)).pow(2).mul(Math.pow(11, 10)).mul(new Decimal(10).pow(level))).mul(applyUpgrade(game.solarSystem.upgrades.jupiter)).max(10),
             }),
         moreGoldenScrap: new MagnetUpgrade(
             level => Utils.roundBase(new Decimal(30 + 10 * level).mul(Decimal.pow(1.07, Math.max(0, level - 20))), 1)
@@ -268,7 +268,7 @@ var game =
             level => game.supernova.cosmicUpgrades.strongerMagnetGS.level > 0 ? new Decimal(1.3).pow(level / 10).add(0.3 * level) : new Decimal(1 + 0.3 * level),
             {
                 getEffectDisplay: effectDisplayTemplates.numberStandard(1, "x"),
-                integral: level => new Decimal(10).mul(Math.pow(100, 20)).mul((Math.log(107) - Math.log(100)) * (level + 3) - 1).mul(new Decimal(107).pow(level)).div(new Decimal(Math.log(107) - Math.log(100)).pow(2).mul(Math.pow(107, 20)).mul(new Decimal(100).pow(level))).mul(applyUpgrade(game.solarSystem.upgrades.jupiter)),
+                integral: level => new Decimal(10).mul(Math.pow(100, 20)).mul((Math.log(107) - Math.log(100)) * (level + 3) - 1).mul(new Decimal(107).pow(level)).div(new Decimal(Math.log(107) - Math.log(100)).pow(2).mul(Math.pow(107, 20)).mul(new Decimal(100).pow(level))).mul(applyUpgrade(game.solarSystem.upgrades.jupiter)).max(10),
             }),
         magnetMergeChance: new MagnetUpgrade(
             level => new Decimal(10 + level * level).mul(Decimal.pow(2, Math.max(0, level - 20)))
@@ -481,7 +481,7 @@ var game =
         mergeTokens: new Decimal(0),
         scrapyard: 1,
         scrapyardProgress: 0,
-        nextDaily: "20220721",
+        nextDaily: 20220721,
         upgrades:
         {
             scrapBoost: new MergeTokenUpgrade(level => Decimal.min(5, 1 + Math.floor(level / 4))
@@ -1339,7 +1339,7 @@ var game =
             }, ["moreFragments"]),
 
             tireValue: new SkillTreeUpgradeFixed([
-                [[new Decimal("1e4002"), RESOURCE_BRICK]],
+                [[new Decimal("1e2103"), RESOURCE_BRICK]],
             ], [false, true], {
                 getEffectDisplay: effectDisplayTemplates.unlock()
             }, ["fasterAutoMerge"]),
@@ -1351,7 +1351,7 @@ var game =
             }, ["scrapBoost2"]),
 
             moreMergeTokens: new SkillTreeUpgradeFixed([
-                [[new Decimal("3000"), RESOURCE_BEAM]],
+                [[new Decimal("2000"), RESOURCE_BEAM]],
             ], [false, true], {
                 getEffectDisplay: effectDisplayTemplates.unlock()
             }, ["higherAstroMax", "tireValue"]),
@@ -1387,7 +1387,8 @@ var game =
                 level => new Decimal(level).mul(100).mul(applyUpgrade(game.solarSystem.upgrades.venus)).div(applyUpgrade(game.scrapUpgrades.fasterBarrels)),
                 {
                     maxLevel: 1,
-                    getEffectDisplay: effectDisplayTemplates.numberStandard(1, "x")
+                    getEffectDisplay: effectDisplayTemplates.numberStandard(1, "x"),
+                    oneDep: true,
                 }, ["higherBeamValueMax", "fasterBricks"]),
 
             unlockMastery: new SkillTreeUpgradeFixed([
@@ -1548,8 +1549,8 @@ var game =
 
             // Row 3
             veryFastCrafting: new SkillTreeUpgradeFixed([
-                [[new Decimal("1e250"), RESOURCE_GS], [new Decimal(1000), RESOURCE_MASTERYTOKEN], [new Decimal(25000), RESOURCE_AEROBEAM], [new Decimal(1000), RESOURCE_WRENCH]],
-                [[new Decimal("1e300"), RESOURCE_GS], [new Decimal(25), RESOURCE_LEGENDARYSCRAP], [new Decimal(10000), RESOURCE_BEAM], [new Decimal(200), RESOURCE_WRENCH]],
+                [[new Decimal("1e250"), RESOURCE_GS], [new Decimal(1000), RESOURCE_MASTERYTOKEN], [new Decimal(25000), RESOURCE_AEROBEAM]],
+                [[new Decimal("1e300"), RESOURCE_GS], [new Decimal(25), RESOURCE_LEGENDARYSCRAP], [new Decimal(10000), RESOURCE_BEAM]],
                     [[new Decimal("1e400"), RESOURCE_GS], [new Decimal(25), RESOURCE_FISHINGNET], [new Decimal(25), RESOURCE_PLASTICBAG]],
                     [[new Decimal("1e750"), RESOURCE_GS], [new Decimal("1e120"), RESOURCE_DARKSCRAP], [new Decimal(250000), RESOURCE_REINFORCEDBEAM]]
             ], [1, 2, 4, 8, 16], {
@@ -2260,7 +2261,7 @@ var game =
                 level => new Decimal(level),
                 {
                     getEffectDisplay: effectDisplayTemplates.numberStandard(1, "+"),
-                    integral: level => new Decimal(level).pow(3).mul(39).sub(new Decimal(level).pow(2).mul(1053).div(2)).add(272 * level),
+                    integral: level => new Decimal(level).pow(3).mul(39).sub(new Decimal(level).pow(2).mul(1053).div(2)).add(272 * level).max(10),
                 }
             ),
             pyxis: new FairyDustUpgrade(
@@ -2268,7 +2269,7 @@ var game =
                 level => new Decimal(1).add(0.1 * level),
                 {
                     getEffectDisplay: effectDisplayTemplates.numberStandard(2, "x"),
-                    integral: level => new Decimal(level).pow(3).mul(2).div(9).sub(new Decimal(level).pow(2).mul(3)).add(5 * level),
+                    integral: level => new Decimal(level).pow(3).mul(2).div(9).sub(new Decimal(level).pow(2).mul(3)).add(5 * level).max(10),
                 }
             ),
             antlia: new FairyDustUpgrade(
@@ -2276,7 +2277,7 @@ var game =
                 level => new Decimal(1).add(0.1 * level),
                 {
                     getEffectDisplay: effectDisplayTemplates.numberStandard(2, "x"),
-                    integral: level => new Decimal(level - 12).mul(level).mul(2 * level - 3).div(9),
+                    integral: level => new Decimal(level - 12).mul(level).mul(2 * level - 3).div(9).max(10),
                 }
             ),
             phoenix: new FairyDustUpgrade(
@@ -2284,7 +2285,7 @@ var game =
                 level => new Decimal(1).add(0.1 * level),
                 {
                     getEffectDisplay: effectDisplayTemplates.numberStandard(2, "x"),
-                    integral: level => new Decimal(level).mul(new Decimal(level).pow(2).mul(2).sub(27 * level).add(54)).div(18),
+                    integral: level => new Decimal(level).mul(new Decimal(level).pow(2).mul(2).sub(27 * level).add(54)).div(18).max(10),
                 }
             ),
             orion: new FairyDustUpgrade(
@@ -2292,7 +2293,7 @@ var game =
                 level => new Decimal(1).add(0.1 * level),
                 {
                     getEffectDisplay: effectDisplayTemplates.numberStandard(2, "x"),
-                    integral: level => new Decimal(level).pow(2).sub(6 * level).add(7).mul(level),
+                    integral: level => new Decimal(level).pow(2).sub(6 * level).add(7).mul(level).max(10),
                 }
             ),
             puppis: new FairyDustUpgrade(
